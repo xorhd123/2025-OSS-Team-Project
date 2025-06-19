5cbb5ab62fc2d6670d3edc71cf8d730fbc10ffa5
DESKTOP-I4H84R7\admin <eunseo040510@naver.com>
2025-06-19 11:20:28 +0900

    잘못 입력시 재입력 요청 추가 -> while

--- edit.py
+++ edit.py
@@ -4,6 +4,18 @@ import os
 
 DATA_FILE = 'data.csv'
 
+
+def load_records(filename=DATA_FILE):
+    records = []
+    if not os.path.exists(filename):
+        return records
+
+    with open(filename, newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f, fieldnames=['date','category','item','amount','type'])
+        for row in reader:
+            records.append(row)
+    return records
+
 def save_records(records, filename=DATA_FILE):
     with open(filename, 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
@@ -26,30 +38,42 @@ def rewrite_record(records):
     print("   수정 가능한 항목   ")
     print("--------------------")
     for idx, r in enumerate(records, start=1):
-        print(f"{idx}: [{r['date']}] {r['type']} - {r['category']} / r['item'] / r['amount']원")
+        print(f"{idx}: [{r['date']}] {r['type']} - {r['category']} / {r['item']} / {r['amount']}원")
     print("--------------------")
 
+while True:
     try:
         i = int(input("수정할 번호를 입력하세요: ").strip())
-        record = records[i-1]
-    except (ValueError, IndexError):
-        print("잘못된 번호입니다")
-        return
+        if 1 <= i <= len(records):
+            record = records[i-1]
+            break
+        else:
+            print(f"잘돗된 번호입니다. 다시 입력해주세요.")
+    except ValueError:
+        print("숫자를 입력해주세요")
     
-    fields = ['date', 'category', 'item', 'amount', 'type']
-    print("수정할 부분을 선택하세요:",",".join(fields))
+fields = ['date', 'category', 'item', 'amount', 'type']
+while True:
+    print("수정할 부분을 선택하세요:")
+    for f in fields:
+        print(f" -{f} (현재: {record[f]})")
     field = input("수정부분: ").strip()
     if field not in fields:
-        print("잘못 입력하셨습니다.")
-        return
+        print("잘못 입력하셨습니다. 다시 선택해주세요.")
+        continue
+    break
     
+while True:
     new_write = input(f"새 {field} 값 입력 (현재: {record[field]}): ").strip()
-    if field == 'amount' and not new_write.isdigit():
-        print("숫자만 입력 가능")
-        return
-    if field == 'type' and new_write not in ('지출', '수입'):
-        print("type은 '지출' 또는 '수입'만 입력 가능합니다")
-        return
+    if field == 'amount':
+        if not new_write.isdigit():
+            print("숫자만 입력 가능합니다.")
+            continue
+    if field == 'type':
+        if new_write not in ('지출', '수입'):
+            print("type은 '지출' 또는 '수입'만 입력 가능합니다")
+            continue
+    break
     
-    record[field] = new_write
-    save_records(records)
+record[field] = new_write
+save_records(records)
