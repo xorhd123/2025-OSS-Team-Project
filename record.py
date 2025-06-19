9426038377d5483862a9291e5d54ca69af4bdfa7
yoon <105162326+Yoo2un@users.noreply.github.com>
2025-06-19 12:50:04 +0900

    항목명 입력 함수

--- record.py
+++ record.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 #수입 지출 함수
 def get_transaction_type():
     while True:
@@ -6,10 +7,15 @@ def get_transaction_type():
         if t_type in ['수입', '지출']:
             return t_type
         print("잘못 입력했습니다. '수입' 또는 '지출' 중에 선택해주세요.")
-#금액 입력
+
+#금액 입력 함수
 def get_amount():
     while True:
         amount = input("금액을 입력하세요 (숫: ").strip()
         if amount.isdigit():
             return amount
         print("숫자만 입력해주세요.")
+
+#항목명 입력
+def get_item():
+    return input("항목명을 입력하세요 (예: 점심, 월급 등): ").strip()
