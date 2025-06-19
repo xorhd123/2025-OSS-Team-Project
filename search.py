3ac4f11eb77a2541339e07cb12f80c6341c1c022
JIYEON <wldus3419@naver.com>
2025-06-19 09:40:18 +0900

    유형 검색 추가

--- search.py
+++ search.py
@@ -11,7 +11,8 @@ def search_result():
 
     print("\n검색 기준을 선택하세요")
     print("1. 날짜로 검색")
-    print("2. 카테고리로 검색")
+    print("2. 유형으로 검색")
+    print("3. 카테고리로 검색")
     choice = input("번호 입력: ").strip()
 
     keyword = input("검색어를 입력하세요: ").strip()
@@ -24,8 +25,12 @@ def search_result():
         if choice == '1' and keyword in r['날짜']:
             print(f"[{r['날짜']}] {r['유형']} - {r['카테고리']} / {r['아이템']} / {r['금액']}원")
             found = True
+        #유형 검색
+        if choice == '1' and keyword in r['유형']:
+            print(f"[{r['날짜']}] {r['유형']} - {r['카테고리']} / {r['아이템']} / {r['금액']}원")
+            found = True
         # 카테고리 검색
-        elif choice == '2' and keyword in r['카테고리']:
+        elif choice == '3' and keyword in r['카테고리']:
             print(f"[{r['날짜']}] {r['유형']} - {r['카테고리']} / {r['아이템']} / {r['금액']}원")
             found = True
 
