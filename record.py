84e05b4f47643dc50f030760894deb635cd88696
xorhd123 <taewon493@gmail.com>
2025-06-21 00:32:29 +0900

    수입 카테고리 추가

--- record.py
+++ record.py
@@ -11,7 +11,7 @@ def get_transaction_type():
 #금액 입력 함수
 def get_amount():
     while True:
-        amount = input("금액을 입력하세요 (숫자: ").strip()
+        amount = input("금액을 입력하세요 (숫자): ").strip()
         if amount.isdigit():
             return amount
         print("숫자만 입력해주세요.")
@@ -21,7 +21,7 @@ def get_item():
     return input("항목명을 입력하세요 (예: 점심, 월급 등): ").strip()
 
 #카테고리 선택 함수
-def select_category():
+def select_category_consume():
     categories = {
         "1": "식비",
         "2": "교통",
@@ -45,8 +45,25 @@ def select_category():
         print("올바른 번호를 입력해주세요 (1~9).")
 
 #수입 항목명 입력
-def input_income_item():
-    return input("수입 항목명을 입력하세요 (예: 월급, 장학금 등): ").strip()
+def select_category_income():
+    categories = {
+        "1": "급여",
+        "2": "용돈",
+        "3": "금융소득",
+        "4": "공적 지원금",
+        "5": "환급",
+        "6": "중고물품 판매"
+    }
+
+    print("\n카테고리를 선택하세요:")
+    for num, name in categories.items():
+        print(f"[{num}] {name}")
+
+    while True:
+        choice = input("카테고리 번호 입력: ").strip()
+        if choice in categories:
+            return categories[choice]
+        print("올바른 번호를 입력해주세요 (1~9).")
 
 #현재 시간
 def get_current_time():
@@ -62,9 +79,9 @@ def get_record():
     amount = get_amount()
 
     if transaction_type == "지출":
-        category = select_category()
+        category = select_category_consume()
     else:  # 수입
-        category = input_income_item()
+        category = select_category_income()
 
     date = get_current_time()
     return [date, transaction_type, category, item, amount]
